0b27b3c3abf251ab158fa7ca77bce9730992d535
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2018-09-18 18:28:50 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html>
+
+<head>
+  <title>Ch 04 - Dynamic Website Example</title>
+  <meta charset='utf-8' />
+  <link rel='stylesheet' href='./css/style.css' type='text/css' />
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+</head>
+
+</html>
